4759089e7f34e582e69f0a75df9e9001004e955a
CD6RJK21 <sanyo3102@gmail.com>
2021-07-30 10:32:10 +0300

    v2 something of something

--- main.py
+++ main.py
@@ -1,35 +1,53 @@
-from tika import parser
+
 import csv
-from PyQt6.QtGui import QFileOpenEvent
 import tkinter as tk
 from tkinter import filedialog
+import fitz
 file_name = filedialog.askopenfilename()
 root = tk.Tk()
 root.withdraw()
 
 # file_name = input()
-raw = parser.from_file(file_name)
-content = raw['content'].split('\n\n\n')
+# raw = parser.from_file(file_name)
+# content = raw['content'].split('\n\n\n')
+# pages = []
+# for page in content:
+#     if page == '':
+#         continue
+#     page = page.split('\n\n')
+#     if len(page) < 5:
+#         continue
+#     if len(page) == 31:
+#         page = page[:-1]
+#     pages.append(page)
+#
 pages = []
-for page in content:
-    if page == '':
-        continue
-    page = page.split('\n\n')
-    if len(page) < 5:
-        continue
-    if len(page) == 31:
-        page = page[:-1]
-    pages.append(page)
-output = []
+with fitz.open(file_name) as doc:
+    for page in doc:
+        text = page.getText()
+        pages.append(page.getText())
+addresses = []
 for page in pages:
-    for address in page:
+    page = page.split('\n')
+    address = []
+    for raw in page:
+        raw = raw.replace('\ufffd', '')
         try:
-            address = address.split('\n')
-            bin = address[-1].split()[-1]
-            state = address[-1].split()[-2]
-            city = address[-1].split()[0].replace(',', '')
-        except IndexError as IE:
-            continue
+            bin_or_not = raw.split()[-1]
+        except IndexError as ie:
+            bin_or_not = ''
+        if '-' not in raw and not bin_or_not.isdecimal():
+            address.append(raw)
+        else:
+            address.append(raw)
+            addresses.append(address)
+            address = []
+output = []
+for address in addresses:
+    try:
+        bin = address[-1].split()[-1]
+        state = address[-1].split()[-2]
+        city = address[-1].split()[0].replace(',', '')
         creating = []
         for i in range(len(address) - 1):
             creating.append(address[i])
@@ -38,7 +56,10 @@ for page in pages:
         creating.append(city)
         creating.append(state)
         creating.append(bin)
-        output.append(creating)
+        if creating != []:
+            output.append(creating)
+    except IndexError as IE:
+        continue
 with open(file_name.replace('.pdf', '.csv'), 'w') as file:
     writer = csv.writer(file, delimiter=";")
     writer.writerow('sep=;')
